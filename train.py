cbb76ec0f22f7402c24a2ba2625a36d4f4b317b6
Zhenyang <zhenyang@example.com>
2018-12-05 11:26:30 +0800

    update train code

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from Tiramisu import Tiramisu
 from keras.callbacks import LearningRateScheduler
 from keras.optimizers import RMSprop, Adam, SGD
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping 
 from keras.layers import Input, merge
 from keras.regularizers import l2
 from keras.models import Model
@@ -24,12 +24,11 @@ from keras.layers.normalization import BatchNormalization
 
 from keras.layers import Conv2D, Conv2DTranspose
 
-#import os
+import os
 config = tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True))
 session = tf.Session(config=config)
 tensorflow_backend.set_session(session)
 K.set_image_dim_ordering('tf')
-#os.environ['CUDA_VISIBLE_DEVICES'] = str(0)
 
 import numpy as np
 import json
@@ -49,6 +48,8 @@ class_weighting = [
  7.3614
 ]
 
+gpu0 = 0
+os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu0)
 
 # load train data
 train_data = np.load('./data/train_data.npy')
@@ -59,6 +60,7 @@ test_label = np.load('./data/val_label.npy')
 
 layer_per_block = [4, 5, 7, 10, 12, 15, 12, 10, 7, 5, 4]
 model = Tiramisu(layer_per_block)
+print(model.summary())
 
 #with open('./weights/prop_tiramisu_weights_67_12_func_10-e7_decay.best.hdf5') as model_file:
  #   model.load_weights(model_file, by_name=False)
@@ -72,22 +74,24 @@ def step_decay(epoch):
     return lrate
 
 lrate = LearningRateScheduler(step_decay)
-optimizer = SGD(lr=0.01)
-#optimizer = Adam(lr=1e-3, decay=0.995)
+#optimizer = SGD(lr=0.01)
+#optimizer = Adam(lr=1e-3, decay=1-0.995)
+optimizer = RMSprop(lr=1e-3, decay=1-0.995)
 
 model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"])
-TensorBoard = callbacks.TensorBoard(log_dir='./logs', histogram_freq=5, write_graph=True, write_images=True)
-filepath="weights/prop_tiramisu_weights_67_12_func_10-e7_decay.best.hdf5"
+logging = TensorBoard(log_dir='./logging', histogram_freq=5, write_graph=True, write_images=True)
+filepath="weights" + "ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5"
 checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=2, save_best_only=True, save_weights_only=False, mode='max')
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=150, verbose=1, mode='auto')
 
-callbacks_list = [checkpoint]
+callbacks_list = [logging, checkpoint, early_stopping]
 
-nb_epoch = 100000
+nb_epoch = 500
 batch_size = 8
 
-history = model.fit(train_data, train_label, batch_size=batch_size, epochs=nb_epoch, callbacks=callbacks_list, class_weight=class_weighting,verbose=1, validation_data=(test_data, test_label), shuffle=True)
+history = model.fit(train_data, train_label, batch_size=batch_size, epochs=nb_epoch, callbacks=callbacks_list, class_weight=class_weighting, verbose=1, validation_data=(test_data, test_label), shuffle=True)
 
-model.save_weights('weights/prop_tiramisu_weights_67_12_func_10-e7_decay{}.hdf5'.format(nb_epoch))
+model.save_weights('weights/RMSprop_tiramisu_weights_103_12_decay{}.hdf5'.format(nb_epoch))
 
 import matplotlib.pyplot as plt
 # list all data in history
